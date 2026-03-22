329e4c29f6a784b9f70efa2c050eac132974a784
legioner9 <legioner9@inbox.ru>
2026-03-22 11:29:55 +0700

    <>

---
+++ .d/.osdn/.bn.this/bn_init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+fn_eargb43634hfdhnffvb(){
+    
+    local bn_dr=${HOME}/edu_lnx/.d/.osdn
+    
+    local dr_all =
+    
+    for dr_all in $(l_dr2e "$bn_dr");do
+        
+        echo "$dr_all"
+    done
+        
+}
+
+fn_eargb43634hfdhnffvb @
